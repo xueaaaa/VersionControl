652a6b02c5fd6f461f6b65315155e60f52f9e676
hnnngn <ss.bazunov@gmail.com>
2024-07-05 23:11:34 +0300

    add: installer

---
+++ Models/Installers/VersionInstaller.cs
@@ -0,0 +1,52 @@
+﻿#pragma warning disable SYSLIB0014
+using System.Net;
+using VersionControl.Models.Versions;
+using Version = VersionControl.Models.Versions.Version;
+
+namespace VersionControl.Models.Installers
+{
+    public class VersionInstaller
+    {
+        private WebClient _webClient;
+        private Version _local;
+        private GitVersion _current;
+
+        public VersionInstaller(Version local, GitVersion git)
+        {
+            _local = local;
+            _current = git;
+            _webClient = new();
+        }
+
+        /// <summary>
+        /// Checks if the version in the repository with the installed version has changed
+        /// </summary>
+        /// <returns>
+        /// True - if the repository version is newer than the local one
+        /// <para></para>
+        /// False - if not
+        /// </returns>
+        public bool Check() =>
+            _current > _local;
+
+        /// <summary>
+        /// Downloads the update file to the path specified in the parameters.
+        /// <para></para>
+        /// <b>Note that this method does not install this update, it only downloads it.</b>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if no update file name or path is specified</exception>
+        public void Install()
+        {
+            var updFileName = Parameters.UpdateFileName;
+            var updFilePath = Parameters.UpdateFilePath
+            if (updFileName == null || updFileName == string.Empty || updFilePath == null || updFilePath == string.Empty)
+                throw new ArgumentNullException();
+
+            _webClient.DownloadFileAsync(new Uri(_current.Assets
+                .Where(v => v.Name == updFileName)
+                .First()
+                .BrowserDownloadUrl), updFilePath);
+        }
+    }
+}
+#pragma warning restore SYSLIB0014
